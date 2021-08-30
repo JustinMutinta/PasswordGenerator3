165c99265ebba1ba6a4eaa953685d98bed70374b
MUTINTA <JustinMutinta@hotmail.com>
2021-08-29 20:49:11 -0500

    1st Pass. Basic outline of GUI

---
+++ src/Password.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Password extends JFrame {
+
+    public Password(){
+        setTitle("Password Generator");             //Set title of Frame
+        setSize(600, 112);              //Set Size. Wanted everything in a long layout from left to right
+        setDefaultCloseOperation(EXIT_ON_CLOSE);    //Close GUI when X is clicked
+
+        setVisible(true);                           //Set to visible
+    }
+}
